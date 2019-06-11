abfd0c2caa8c28053a56857f68efbd578eed31b0
jisung <jisung.smart@gmail.com>
2019-06-11 15:45:25 +0900

    Add Details View controller

---
+++ Refrig/Controller/DeatilsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DeatilsViewController.swift
+//  Refrig
+//
+//  Created by The book on 11/06/2019.
+//  Copyright © 2019 The book. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailsViewController {
+    
+}
