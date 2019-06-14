a9802e1332b12e1f9b07dcee400b2d4648237346
jisung <jisung.smart@gmail.com>
2019-06-14 13:27:14 +0900

    Add alertViewController prototype

---
+++ Refrig/Controller/AlertViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AlertViewController.swift
+//  Refrig
+//
+//  Created by The book Air on 14/06/2019.
+//  Copyright © 2019 The book. All rights reserved.
+//
+
+import Foundation
+
+class AlertViewController {
+    
+}
