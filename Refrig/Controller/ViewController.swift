7706fed5c95f36d8ffc60fb171679aed0d9289bc
jisung <jisung.smart@gmail.com>
2019-07-01 16:30:01 +0900

    In the view controller add alert method and implement function when push add button pop up new button

--- Refrig/Controller/ViewController.swift
+++ Refrig/Controller/ViewController.swift
@@ -11,10 +11,28 @@ import CoreData
 
 
 class StorageViewController: UIViewController {
+    
+    
+    var category = [Storage]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    //When touch outside of keyboard it reset the position
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    //when Retrun input occuerd the keyboard is reset
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
     }
     
     
@@ -23,6 +41,71 @@ class StorageViewController: UIViewController {
         print("Success")
     }
     
+    //Data Manipulation
+    
+    func saveCategory(){
+        do {
+            try context.save()
+        }catch{
+            print("Error saving category \(error)")
+        }
+    }
+    
+    func loadCategory(){
+        let request : NSFetchRequest<Storage> = Storage.fetchRequest()
+        
+        do{
+            category = try context.fetch(request)
+        } catch {
+            print("Error loading category \(error)")
+        }
+    }
+    
+    
+    //MARK:- Alert Action
+    
+    @IBAction func Add(_ sender: Any) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Storage", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            let newCategory = Storage(context: self.context)
+            newCategory.storageName = textField.text!
+            
+            // var button   = UIButton.buttonWithType(UIButtonType.System) as UIButton
+            let button = UIButton(type: .system) // let preferred over var here
+            button.frame = CGRect(x: 100,y: 100,width: 100,height: 100)
+            button.backgroundColor = UIColor.green
+            button.setTitle(newCategory.storageName, for: UIControl.State.init())
+            //button.addTarget(self, action: "Action:", for: UIControl.Event.touchUpInside)
+            self.view.addSubview(button)
+            
+            self.category.append(newCategory)
+            
+            
+            
+            self.saveCategory()
+        }
+        
+        alert.addAction(action)
+        
+        alert.addTextField { (field) in
+            textField = field
+            textField.placeholder = "Add New Category"
+        }
+        
+        present(alert, animated: true) {
+            alert.view.superview?.isUserInteractionEnabled = true
+            alert.view.superview?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.alertControllerBackgroundTapped)))
+        }
+    }
+    
+    @objc func alertControllerBackgroundTapped(){
+        self.dismiss(animated: true, completion: nil)
+    }
  
 }
 
